6b93dd9e6f492893a3625b7dc0f8e958a5f44d7a
lqdev <liquidekgaming@gmail.com>
2021-04-13 18:53:45 +0200

    better

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ struct AudioState {
     stream: Stream,
     samples_back: Arc<Mutex<Vec<f32>>>,
     samples_front: Vec<f32>,
+    last_sample: (f32, f32),
 }
 
 impl AudioState {
@@ -51,7 +52,14 @@ impl AudioState {
             stream,
             samples_back: arc_samples.clone(),
             samples_front: Vec::new(),
+            last_sample: (0.0, 0.0),
         })
+    }
+
+    fn get_xy(half_width: f32, half_height: f32, left: f32, right: f32) -> (i32, i32) {
+        let x = left * half_width + half_width;
+        let y = -right * half_height + half_height;
+        (x as i32, y as i32)
     }
 
     fn render(&mut self, mut plot: Plot) {
@@ -66,68 +74,120 @@ impl AudioState {
             }
         }
         // plot the samples
-        plot.clear();
         for channels in self.samples_front.chunks_exact(2) {
             if let [left, right, ..] = channels {
-                let x = left * half_width + half_width;
-                let y = -right * half_height + half_height;
-                plot.dot(x, y);
+                let (previous_left, previous_right) = self.last_sample;
+                let (previous_x, previous_y) = Self::get_xy(half_width, half_height, previous_left, previous_right);
+                let (x, y) = Self::get_xy(half_width, half_height, *left, *right);
+                plot.dot(previous_x, previous_y, x, y);
+                self.last_sample = (*left, *right);
             }
         }
+        plot.done();
     }
 }
 
-struct Plot<'a> {
+struct Plot<'a, 'b> {
     width: u32,
     height: u32,
-    pixels: &'a mut [u8]
+    pixels: &'a mut [u8],
+    plot: &'b mut [u8],
+    previous_pos1: (i32, i32)
 }
 
-impl Plot<'_> {
-    fn clear(&mut self) {
+const fn to_u8(x: i32) -> u8 {
+    if x < 0 { 0 }
+    else if x > 255 { 255 }
+    else { x as u8 }
+}
+
+impl Plot<'_, '_> {
+    const INTENSITY_COLORS: [(u8, u8, u8); 256] = {
+        let mut colors = [(0u8, 0u8, 0u8); 256];
+        let mut i = 0;
+        while i < 256 {
+            let n = i as i32;
+            colors[i].1 = to_u8(n * 2);
+            let rb = to_u8((n - 128) * 2);
+            colors[i].0 += rb;
+            colors[i].2 += rb;
+            i += 1;
+        }
+        colors
+    };
+
+    fn plot_index(&self, x: u32, y: u32) -> usize {
+        (x + y * self.width) as usize
+    }
+
+    fn pixel_index(&self, x: u32, y: u32) -> usize {
+        self.plot_index(x, y) * 4
+    }
+
+    fn done(&mut self) {
         const DIVISIONS: u32 = 5;
+        const DIV_BRIGHTNESS: u8 = 24;
         let division_width = self.width / DIVISIONS;
         let division_height = self.height / DIVISIONS;
-        for x in 0..self.width {
+        let half_division_width = division_width / 2;
+        let half_division_height = division_height / 2;
+
+        // background
+        for pixel in self.pixels.iter_mut() {
+            *pixel = 0;
+        }
+        for div in 0..DIVISIONS {
+            let x = half_division_width + division_width * div;
             for y in 0..self.height {
-                let i = ((x + y * self.width) * 4) as usize;
-                let (r, g, b, a) = (i, i + 1, i + 2, i + 3);
-                if (x + division_width / 2) % division_width == 0 || (y + division_height / 2) % division_height == 0 {
-                    const BRIGHTNESS: u8 = 32;
-                    self.pixels[r] = BRIGHTNESS;
-                    self.pixels[g] = BRIGHTNESS;
-                    self.pixels[b] = BRIGHTNESS;
-                } else {
-                    self.pixels[r] = 0;
-                    self.pixels[g] = 0;
-                    self.pixels[b] = 0;
-                }
-                self.pixels[a] = 255;
+                let i = self.pixel_index(x, y);
+                self.pixels[i] = DIV_BRIGHTNESS;
+                self.pixels[i + 1] = DIV_BRIGHTNESS;
+                self.pixels[i + 2] = DIV_BRIGHTNESS;
             }
+            let y = half_division_height + division_height * div;
+            for x in 0..self.width {
+                let i = self.pixel_index(x, y);
+                self.pixels[i] = DIV_BRIGHTNESS;
+                self.pixels[i + 1] = DIV_BRIGHTNESS;
+                self.pixels[i + 2] = DIV_BRIGHTNESS;
+            }
+        }
+
+        // dots
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let i = self.pixel_index(x, y);
+                let intensity = self.plot[self.plot_index(x, y)];
+                let color = Self::INTENSITY_COLORS[intensity as usize];
+                self.pixels[i] = self.pixels[i].saturating_add(color.0);
+                self.pixels[i + 1] = self.pixels[i + 1].saturating_add(color.1);
+                self.pixels[i + 2] = self.pixels[i + 2].saturating_add(color.2);
+            }
+        }
+
+        // fading out
+        for plot in self.plot.iter_mut() {
+            *plot = (*plot as f32 * 0.85) as u8;
         }
     }
 
-    fn pixel(&mut self, x: f32, y: f32) {
-        if (0.0..self.width as f32).contains(&x) && (0.0..self.height as f32).contains(&y) {
+    fn pixel(&mut self, x: i32, y: i32) {
+        if (0..self.width as i32).contains(&x) && (0..self.height as i32).contains(&y) {
             let (x, y) = (x as u32, y as u32);
-            let i = ((x + y * self.width) * 4) as usize;
-            let (r, g, b, a) = (i, i + 1, i + 2, i + 3);
-            self.pixels[g] = self.pixels[g].saturating_add(96);
-            let intensity = ((self.pixels[g] as f32 - 192.0).max(0.0) * 2.0) as u8;
-            self.pixels[r] = intensity;
-            self.pixels[b] = intensity;
-            self.pixels[a] = 255;
+            let i = (x + y * self.width) as usize;
+            self.plot[i] = self.plot[i].saturating_add(32);
         }
     }
 
-    fn dot(&mut self, x: f32, y: f32) {
-        const R: i32 = 1;
-        let (x, y) = (x as i32, y as i32);
-        for yy in y - R..y + R {
-            for xx in x - R..x + R {
-                self.pixel(xx as f32, yy as f32);
+    fn dot(&mut self, x0: i32, y0: i32, x1: i32, y1: i32) {
+        let (x0, y0) = (x0 as i32, y0 as i32);
+        let (x1, y1) = (x1 as i32, y1 as i32);
+        for (x, y) in line_drawing::Bresenham::new((x0, y0), (x1, y1)) {
+            if (x, y) != self.previous_pos1 {
+                self.pixel(x, y);
             }
         }
+        self.previous_pos1 = (x1, y1);
     }
 }
 
@@ -143,6 +203,8 @@ fn main() -> anyhow::Result<()> {
     let mut pixels = Pixels::new(window_size.width, window_size.height, surface_texture)?;
 
     let mut state = AudioState::new()?;
+    let mut plot = Vec::new();
+    plot.resize((window_size.width * window_size.height) as usize, 0);
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Poll;
@@ -168,9 +230,14 @@ fn main() -> anyhow::Result<()> {
                 state.render(Plot {
                     width: window_size.width,
                     height: window_size.height,
-                    pixels: pixels.get_frame()
+                    pixels: pixels.get_frame(),
+                    plot: &mut plot,
+                    previous_pos1: (0, 0),
                 });
-                pixels.render().unwrap();
+                match pixels.render() {
+                    Err(x) => eprintln!("{}", x),
+                    _ => (),
+                }
             },
 
             _ => (),
